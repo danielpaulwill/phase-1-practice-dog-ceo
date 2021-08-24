d51c7f92ffbf141f8d6db46dde74ef310f4f7e72
Daniel Williams <danielpaulwill@gmail.com>
2021-08-23 21:21:03 -0400

    forEach is not a function error

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 console.log('%c HI', 'color: firebrick')
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', fetchBoyFetch);
+
+function fetchBoyFetch() {
+  init4()
+  initAll()
+};
 
 let imageContainer = document.getElementById('dog-image-container');
 
-function init() {
+function init4() {
   fetch("https://dog.ceo/api/breeds/image/random/4")
   .then(res => res.json())
-  .then(data => imageCreation(data.message))
-};
-
-function imageCreation(arg1) {
-  arg1.forEach(imageUrl => {
+  .then(data => {
+  data.message.forEach(imageUrl => {
     let dogImage = document.createElement('img')
     dogImage.src = imageUrl
     document.getElementById('dog-image-container').appendChild(dogImage)
-
-    })
+  })
+})
 };
 
 
 
-let imgContain = document.querySelector('#dog-image-container')
 
+
+
+
+
+function initAll() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(res => res.json())
+  .then(data => {
+  data.message.forEach(dogBreed => {   //Iterate through each dog breed
+  let breedList = document.createElement('li')   //Create li element for each breed
+  let breedText = document.createTextNode(`${dogBreed}`)   //Put each breed into a text node
+  breedList.appendChild(breedText)   //Put each breed text node into it's li element
+  dogBreedList.appendChild(breedList)   //Put each li element into the ul DOM element
+    })
+  })
+};
